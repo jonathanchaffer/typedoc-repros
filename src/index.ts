fbafe75a97b42db66f4b91b4f06bf78347aa3e28
Jonathan Chaffer <jonathanchaffer@gmail.com>
2023-01-30 08:57:26 -0500

    Update repro

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 /**
- * Should be documented.
+ * Arrow function not being correctly included in documentation.
  */
-export const foo = (bar: string): boolean => {
+export const foo = (foo: string): boolean => {
   return true;
 };
+
+/**
+ * Non-arrow function being correctly included.
+ */
+export function bar(bar: string): boolean {
+  return true;
+}
